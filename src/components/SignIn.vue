0bdf29a2ba9b8ca0b2fdd9eeb070d526eecf0eb1
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-09-02 22:30:42 -0300

    feat:add SingIn page

---
+++ src/components/SignIn.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="container mx-auto flex flex-col justify-center items-center h-full"
+  >
+    <h1 class="text-4xl font-bold mb-5 text-primaryDarker">Sign In</h1>
+    <form class="flex-col justify-center items-center">
+      <input
+        type="email"
+        placeholder="E-mail"
+        class="w-full text-sm mb-4 pl-3 rounded border py-2 shadow-md"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        class="text-sm w-full mb-4 pl-3 rounded border py-2 shadow-md"
+      />
+      <button
+        type="submit"
+        class="font-bold text-white bg-primary w-full py-2 rounded-md shadow-2xl hover:bg-azul-hover duration-150"
+      >
+        Sign In
+      </button>
+    </form>
+  </div>
+</template>
+
+<style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+}
+</style>
