0eb341c5c02e69cee393b969407828299320da63
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-09-03 22:31:56 -0300

    fix:localstorage and style

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,32 +1,41 @@
 <template>
-  <div
-    class="container mx-auto flex flex-col justify-center items-center h-full"
-  >
-    <h1 class="text-4xl font-bold mb-5 text-primaryDarker">Sign In</h1>
-    <form class="flex-col justify-center items-center" @submit.prevent="login">
-      <input
-        type="email"
-        placeholder="E-mail"
-        class="w-full text-sm mb-4 pl-3 rounded border py-2 shadow-md"
-        v-model="form.email"
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        class="text-sm w-full mb-4 pl-3 rounded border py-2 shadow-md"
-        v-model="form.password"
-        required
-      />
+  <div class="h-screen w-screen flex-row">
+    <div
+      class="w-1/2 h-full flex bg-primaryDarker justify-center items-center flex-col"
+    >
+      <div class="basis-5/12 p-6">
+        <img src="../assets/videotopia.png" />
+        <form @submit.prevent="login">
+          <label for="email" class="text-walterWhite">Email:</label>
+          <input
+            type="email"
+            placeholder="E-mail"
+            class="w-full text-sm mb-4 pl-3 rounded border py-2 shadow-md"
+            v-model="form.email"
+            required
+          />
+          <label for="password" class="text-walterWhite">Password:</label>
+          <input
+            type="password"
+            placeholder="Password"
+            class="text-sm w-full mb-5 pl-3 rounded border py-2 shadow-md"
+            v-model="form.password"
+            required
+          />
 
-      <button
-        type="submit"
-        class="font-bold text-white bg-primary w-full py-2 rounded-md shadow-2xl hover:bg-azul-hover duration-150"
-      >
-        Sign In
-      </button>
-    </form>
-    <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+          <button
+            type="submit"
+            class="font-bold text-white bg-primary w-full py-2 rounded-md shadow-2xl hover:bg-primaryLighter-hover duration-150"
+          >
+            Sign In
+          </button>
+        </form>
+        <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+      </div>
+    </div>
+    <div
+      class="bg-search-background bg-cover bg-no-repeat w-[850px] h-[450px]"
+    ></div>
   </div>
 </template>
 
@@ -60,11 +69,23 @@ export default {
 
   methods: {
     login() {
+      // Mocked user data
       const mockedUser = {
         email: "user@example.com",
         password: "password123",
       };
-      console.log(mockedUser);
+      if (
+        this.form.email === mockedUser.email &&
+        this.form.password === mockedUser.password
+      ) {
+        // Store user information in Local Storage
+        localStorage.setItem("user", JSON.stringify(mockedUser));
+        // Redirect to the feed
+        this.$router.push("/feed-videos");
+      } else {
+        this.errorMessage = "Invalid email or password. Please try again.";
+        this.$router.push("/SignIn");
+      }
     },
   },
 };
