5760b5927797a4b6b21ae799eb674f168db14b04
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-09-03 22:31:24 -0300

    feat:add routes to the project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,11 @@
 <template>
-  <div class="h-full">
-    <SignIn />
+  <div class="bg-purpleDark">
+    <router-view />
   </div>
 </template>
 
 <script>
-import SignIn from "./components/SignIn.vue";
-
 export default {
   name: "App",
-  components: {
-    SignIn,
-  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
