d3e32e9b90e4b30f93ebed02474ecbad34d50f38
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-09-04 22:31:37 -0300

    feat:add footer component to the components file

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+    <div
+      className=" container mx-auto w-full h-[1px] bg-grayLighter mt-10 mb-3"
+    ></div>
+    <div
+      className="p-5 flex flex-col justify-center items-center bg-purpleDark"
+    >
+      <img
+        src="../assets/videotopia.png"
+        alt="logo footer"
+        height="30"
+        width="250"
+      />
+      <p className="mt-1 font-medium text-sm text-walterWhite">
+        Todos os direitos reservados!
+      </p>
+      <p className="mt-1 font-normal text-sm text-walterWhite">
+        Desenvolvido por Bruna Eduarda, 2023
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter",
+};
+</script>
