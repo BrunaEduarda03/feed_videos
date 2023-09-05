238e14b1f3b705f693d0a901ab54839cdfed54aa
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-09-05 16:29:31 -0300

    feat:add loading spinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="isLoading" class="text-walterWhite">
+    <font-awesome-icon icon="circle-notch" spin class="text-walterWhite" />
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+
+export default {
+  name: "LoadingSpinner",
+  components: {
+    FontAwesomeIcon,
+  },
+  props: {
+    isLoading: Boolean,
+  },
+  data() {
+    return {
+      icons: {
+        faCircleNotch,
+      },
+    };
+  },
+};
+</script>
