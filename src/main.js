bee5275ca39138b2a2f7a2671fea8e035d3b6495
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-09-04 23:18:38 -0300

    feat:add fontfawesome for icons

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./css/main.css";
 import router from "./routes";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).use(router).mount("#app");
+library.add(fas);
+
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
