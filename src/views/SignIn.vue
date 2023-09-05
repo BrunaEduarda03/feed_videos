ee3df1aed2fed15cbe042e2d34187173cafb576c
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-09-05 16:34:10 -0300

    style:implemented responsiveness

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,51 +1,53 @@
 <template>
-  <div class="h-screen w-screen flex-row">
+  <div class="h-screen w-screen flex flex-col lg:flex-row">
     <div
-      class="w-1/2 h-full flex bg-primaryDarker justify-center items-center flex-col"
+      class="h-full lg:w-1/2 flex bg-primaryDarker justify-center items-center flex-col p-6 min-w-screen"
     >
-      <div class="basis-5/12 p-6">
-        <img src="../assets/videotopia.png" alt="logo" class="mb-10" />
-        <form @submit.prevent="login">
-          <label for="email" class="text-walterWhite">Email:</label>
-          <input
-            type="email"
-            placeholder="hello@reallygreatsite.com"
-            class="w-full text-sm mb-3 pl-3 rounded border py-2 shadow-md mt-1 bg-transparent text-walterWhite"
-            v-model="form.email"
-            required
-          />
-          <label for="password" class="text-walterWhite">Password:</label>
-          <input
-            type="password"
-            placeholder="*******"
-            class="text-sm w-full mb-5 mt-1 pl-3 rounded border py-2 shadow-md bg-transparent text-walterWhite"
-            v-model="form.password"
-            required
-          />
+      <img src="../assets/videotopia.png" alt="logo" class="mb-10" />
+      <form @submit.prevent="login">
+        <label for="email" class="text-walterWhite">Email:</label>
+        <input
+          type="email"
+          placeholder="hello@reallygreatsite.com"
+          class="w-full text-sm mb-3 pl-3 rounded border py-2 shadow-md mt-1 bg-transparent text-walterWhite"
+          v-model="form.email"
+          required
+        />
+        <label for="password" class="text-walterWhite">Password:</label>
+        <input
+          type="password"
+          placeholder="*******"
+          class="text-sm w-full mb-5 mt-1 pl-3 rounded border py-2 shadow-md bg-transparent text-walterWhite"
+          v-model="form.password"
+          required
+        />
 
-          <button
-            type="submit"
-            class="font-bold text-white bg-primary w-full py-2 rounded-md shadow-2xl hover:bg-primaryLighter-hover duration-150"
-          >
-            Sign In
-          </button>
-          <div class="flex justify-center mt-2">
-            <p class="text-walterWhite">
-              New here?
-              <span class="underline font-bold text-walterWhite"
-                >Create an account</span
-              >
-            </p>
-          </div>
-        </form>
-        <p v-if="errorMessage" class="text-redPrimary flex justify-center">
-          {{ errorMessage }}
-        </p>
-      </div>
+        <button
+          type="submit"
+          class="font-bold text-white bg-primary w-full py-2 rounded-md shadow-2xl hover:bg-primaryLighter-hover duration-150"
+        >
+          Sign In
+        </button>
+        <div class="flex justify-center mt-2">
+          <p class="text-walterWhite">
+            New here?
+            <span class="underline font-bold text-walterWhite"
+              >Create an account</span
+            >
+          </p>
+        </div>
+      </form>
+      <p v-if="errorMessage" class="text-redPrimary flex justify-center">
+        {{ errorMessage }}
+      </p>
     </div>
-    <div
-      class="flex flex-col w-7/12 bg-search-background bg-cover h-[600px]"
-    ></div>
+    <div class="flex flex-col w-1/2 justify-center items-center bg-contain">
+      <img
+        src="../assets/ballImage.png"
+        alt="background"
+        class="h-auto w-full hidden lg:block p-16"
+      />
+    </div>
   </div>
 </template>
 
