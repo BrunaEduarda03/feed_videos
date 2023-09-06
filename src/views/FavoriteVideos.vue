19ce3eca097c7a5c4f291427141a37ec6ecff295
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-09-06 16:11:17 -0300

    problem:footer favorite-videos

--- src/views/FavoriteVideos.vue
+++ src/views/FavoriteVideos.vue
@@ -2,18 +2,16 @@
   <div class="bg-purpleDark container mx-auto p-5 flex flex-col">
     <Header />
     <div className=" mt-5 items-center grow">
-      <p className=" text-primaryLighter mt-5 text-xl  ">
+      <p className=" text-primaryLighter mt-5 text-xl mb-5 ">
         Você não possui nenhum video Favorito!
       </p>
-      <div className=" flex flex-col  mt-5 ">
-        <router-link to="/feed-videos">
-          <button
-            class="bg-primaryDarker hover:bg-pinkLighter text-white font-semibold py-2 px-4 rounded-md"
-          >
-            Voltar para a página inicial
-          </button>
-        </router-link>
-      </div>
+      <router-link to="/feed-videos">
+        <button
+          class="bg-primaryDarker hover:bg-pinkLighter text-white font-semibold py-2 px-4 rounded-md"
+        >
+          Voltar para a página inicial
+        </button>
+      </router-link>
     </div>
     <Footer />
   </div>
