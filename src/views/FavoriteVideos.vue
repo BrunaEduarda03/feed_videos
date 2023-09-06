633356dbaa81eff7d4f769b486d3695621f19e6a
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-09-06 00:54:16 -0300

    style:improved component favorite videos

--- src/views/FavoriteVideos.vue
+++ src/views/FavoriteVideos.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="h-screen">
+  <div class="bg-purpleDark h-screen container mx-auto p-5">
     <Header />
+    <div className=" flex flex-col mt-5 ">
+      <p className=" text-primaryLighter mt-5 text-xl ">
+        Você não possui nenhum video Favorito!
+      </p>
+      <div className=" flex flex-col  mt-5 ">
+        <router-link to="/feed-videos">
+          <button
+            class="bg-primaryDarker hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
+          >
+            Voltar para a página inicial
+          </button>
+        </router-link>
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
